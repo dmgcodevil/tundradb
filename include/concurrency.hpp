5cb089644c6d635cfd715bb3ca93da0c5f8018dc
dmgcodevil <dmgcodevil@gmail.com>
2025-03-31 16:54:48 -0400

    wrapper around tbb::concurrent_hash_map

--- include/concurrency.hpp
+++ include/concurrency.hpp
@@ -4,174 +4,41 @@
 #include <tbb/concurrent_hash_map.h>
 #include <tbb/concurrent_set.h>
 
+#include <iostream>
 #include <set>
-
 namespace tundradb {
 
 template <typename T>
 struct RCUConcurrentSet {
-  tbb::concurrent_hash_map<T, bool> data_;  // false means element is deleted
-  std::atomic<size_t> remove_counter;
-  std::shared_ptr<std::set<T>> snapshot_;
-  std::atomic<bool> generate_snapshot_al_;  // snapshot generation in progress
-  std::atomic<int64_t> version_;
-  std::atomic<int64_t> snapshot_version_;
-  std::vector<std::shared_ptr<std::set<T>>> old_snapshots;
-  int64_t compaction_on_modification_ops_;
-
-  RCUConcurrentSet(int64_t compaction_on_modification_ops = 1000)
-      : remove_counter(0),
-        snapshot_(std::make_shared<std::set<T>>()),
-        generate_snapshot_al_(false),
-        version_(0),
-        snapshot_version_(0),
-        compaction_on_modification_ops_(compaction_on_modification_ops) {}
-
+  tbb::concurrent_hash_map<T, std::monostate> data_;
   bool insert(T t) {
-    bool inserted = false;
-    {
-      typename tbb::concurrent_hash_map<T, bool>::accessor acc;
-
-      if (data_.insert(acc, t)) {
-        acc->second = true;
-        version_.fetch_add(1, std::memory_order_release);
-        inserted = true;
-      } else {
-        inserted = !acc->second;
-        acc->second = true;
-        if (inserted) {
-          version_.fetch_add(1, std::memory_order_release);
-        }
-      }
+    typename tbb::concurrent_hash_map<T, std::monostate>::accessor acc;
+    if (data_.insert(acc, t)) {
+      return true;
     }
-    // trigger_generate_snapshot_(); do don't need to run snap generation on insert
-    return inserted;
-  }
-
-  bool remove(T t) {
-    bool removed = false;
-    {
-      typename tbb::concurrent_hash_map<T, bool>::accessor acc;
-      if (data_.find(acc, t) && acc->second) {
-        remove_counter.fetch_add(1, std::memory_order_release);
-        acc->second = false;
-        version_.fetch_add(1, std::memory_order_release);
-        removed = true;
-      }
-    }
-    trigger_generate_snapshot_();
-    return removed;
+    return false;
   }
 
   bool contains(T t) {
-    typename tbb::concurrent_hash_map<T, bool>::const_accessor acc;
-    return data_.find(acc, t) && acc->second;
+    typename tbb::concurrent_hash_map<T, std::monostate>::const_accessor acc;
+    return data_.find(acc, t);
   }
-
-  // Clean up old snapshots that are no longer in use
-  void cleanup_old_snapshots() {
-    // Remove snapshots that only we reference (use_count == 1)
-    auto it = std::remove_if(old_snapshots.begin(), old_snapshots.end(),
-                             [](const std::shared_ptr<std::set<T>>& ptr) {
-                               if (ptr.use_count() == 1) {
-                                 // Clear the contents to free memory
-                                 // immediately
-                                 ptr->clear();
-                                 return true;  // Remove from vector
-                               }
-                               return false;  // Keep in vector
-                             });
-
-    old_snapshots.erase(it, old_snapshots.end());
-  }
-
-  // eventually consistent
-  int64_t size() const {
-    auto current_snapshot_version =
-        snapshot_version_.load(std::memory_order_acquire);
-    auto current_version = version_.load(std::memory_order_acquire);
-
-    if (current_snapshot_version >= current_version) {
-      return snapshot_->size();
+  bool remove(T t) {
+    if (data_.erase(t)) {
+      return true;
     }
-    auto total = data_.size();
-    auto removed = remove_counter.load(std::memory_order_acquire);
-    return (total > removed) ? (total - removed) : 0;
+    return false;
   }
 
-  void trigger_generate_snapshot_() {
-    auto current_snapshot_version =
-        snapshot_version_.load(std::memory_order_acquire);
-    auto current_version = version_.load(std::memory_order_acquire);
-    if (current_snapshot_version < current_version &&
-        current_version - current_snapshot_version >=
-            compaction_on_modification_ops_) {
-      generate_snapshot_();
+  int64_t size() const { return data_.size(); }
+
+  std::set<T> get_all() {
+    std::set<T> key_set;
+    for (auto it = data_.begin(); it != data_.end(); ++it) {
+      key_set.insert(it->first);
     }
+    return key_set;
   }
-
-  std::shared_ptr<std::set<T>> generate_snapshot_() {
-    auto current_snapshot_version =
-        snapshot_version_.load(std::memory_order_acquire);
-    auto current_version = version_.load(std::memory_order_acquire);
-
-    if (current_snapshot_version >= current_version) {
-      return snapshot_;
-    }
-
-    bool expected = false;
-    if (generate_snapshot_al_.compare_exchange_strong(
-            expected, true, std::memory_order_acq_rel,
-            std::memory_order_acquire)) {
-      try {
-        current_snapshot_version =
-            snapshot_version_.load(std::memory_order_acquire);
-        current_version = version_.load(std::memory_order_acquire);
-
-        if (current_snapshot_version < current_version) {
-          std::set<T> selected;
-          std::vector<T> to_delete;
-          typename tbb::concurrent_hash_map<T, bool>::range_type range =
-              data_.range();
-          for (auto it = range.begin(); it != range.end(); ++it) {
-            if (it->second) {
-              selected.insert(it->first);
-            } else {
-              to_delete.push_back(it->first);
-            }
-          }
-
-          size_t deleted_count = 0;
-          for (const auto& key : to_delete) {
-            if (data_.erase(key)) {
-              deleted_count++;
-            }
-          }
-
-          if (deleted_count > 0) {
-            remove_counter.fetch_sub(deleted_count, std::memory_order_release);
-          }
-
-          old_snapshots.push_back(snapshot_);
-
-          auto new_snapshot =
-              std::make_shared<std::set<T>>(std::move(selected));
-          snapshot_ = new_snapshot;
-          snapshot_version_.store(current_version, std::memory_order_release);
-          cleanup_old_snapshots();
-        }
-      } catch (...) {
-        generate_snapshot_al_.store(false, std::memory_order_release);
-        throw;
-      }
-
-      generate_snapshot_al_.store(false, std::memory_order_release);
-    }
-
-    return snapshot_;
-  }
-
-  std::shared_ptr<std::set<T>> get_all() { return generate_snapshot_(); }
 };
 
 }  // namespace tundradb
