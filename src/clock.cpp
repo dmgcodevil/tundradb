3f3ba1098e61216defe4e69de5dd1cd94025b0cd
dmgcodevil <dmgcodevil@gmail.com>
2026-02-10 22:49:55 -0500

    clock

---
+++ src/clock.cpp
@@ -0,0 +1,14 @@
+#include "../include/clock.hpp"
+
+namespace tundradb {
+
+// Static initialization
+std::atomic<Clock*> Clock::instance_{nullptr};
+
+// Default SystemClock instance (singleton)
+Clock& Clock::default_instance() {
+  static SystemClock system_clock;
+  return system_clock;
+}
+
+}  // namespace tundradb
